8809fb6d075df0005a2fab083778ebfe3c8a37a2
leicaflorian <florian.leica@gmail.com>
2022-08-15 00:40:43 +0200

    Updates Rai channel for italy region

--- app/Http/Controllers/RaiController.php
+++ app/Http/Controllers/RaiController.php
@@ -4,11 +4,8 @@ namespace App\Http\Controllers;
 
 use Adrianorosa\GeoLocation\GeoLocation;
 use App\Traits\WebserverOneController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Str;
-use PHPHtmlParser\Dom;
-use PHPHtmlParser\Options;
+use Jenssegers\Agent\Agent;
+
 
 class RaiController extends Controller {
   use \App\Traits\ChannelController, WebserverOneController {
@@ -23,9 +20,12 @@ class RaiController extends Controller {
   public function stream($channel) {
     $userIp  = request()->ip();
     $details = GeoLocation::lookup($userIp);
+    $agent = new Agent();
+  
+    $browser = $agent->browser();
     
-    if ($details->getCountryCode() === "IT") {
-      return redirect()->action([RaiPlayController::class, 'stream']);
+    if ($details->getCountryCode() === "IT" && !$browser) {
+      return redirect()->action([RaiPlayController::class, 'stream'], ["channel" => $channel]);
     }
     
     return $this->traitStream($channel);

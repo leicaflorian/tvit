d55f4770b8a8f2faa72926d00885eccf599411ca
leicaflorian <florian.leica@gmail.com>
2023-12-18 14:34:09 +0100

    Adds proxy to pultoTv scrapper

---
+++ app/Classes/ProxiedHttp.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Classes;
+
+use GuzzleHttp\Client;
+use Illuminate\Support\Str;
+
+class ProxiedHttp {
+  private array $availableProxies = [
+    "it" => "http://35l1IZBDuKp0Lbek:buXDFrlivy7ljTWo_country-it@geo.iproyal.com:12321",
+    "ro" => ""
+  ];
+  private Client $client;
+  
+  function __construct(string $country) {
+    $this->client = new Client([
+      "proxy"  => [
+        'https' => $this->availableProxies[$country],
+      ],
+      'verify' => false
+    ]);
+  }
+  
+  function call($url, $method, $options) {
+    $resp = $this->client->request($method, $url, $options);
+    
+    $body = $resp->getBody()->getContents();
+    
+    // se header application/json allora ritorna json
+    if (Str::contains($resp->getHeader("Content-Type")[0], "application/json")) {
+      return json_decode($body, true);
+    }
+    
+    return $body;
+  }
+  
+  static function get($country, $url, $options = []) {
+    $client = new ProxiedHttp($country);
+    
+    return $client->call($url, "GET", $options);
+  }
+  
+  
+}
